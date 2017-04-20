38af7c05c3a6ce08fe0ea6fefca16e8f7113823f
Franklin Ross <fhross@gmail.com>
2017-04-21 01:55:39 +1000

    Added mocha testing with chai

---
+++ mocha.config.js
@@ -0,0 +1,12 @@
+require("ts-node").register({
+  compilerOptions: {
+    "moduleResolution": "node",
+    "module": "commonjs",
+    "alwaysStrict": true,
+    "noImplicitAny": false,
+    "noImplicitThis": true,
+    "removeComments": false,
+    "strictNullChecks": true,
+    "pretty": true
+  }
+});
---
+++ src/main.test.ts
@@ -0,0 +1,8 @@
+import 'mocha';
+import { expect } from "chai";
+
+describe("tests", function() {
+  it("should be written", function () {
+    expect(true).to.be.true;
+  });
+});
